4abf5ad96ab8f6d22786188fa3ce46c7add9f56e
Hussem Ben Belgacem <hussem.benbelgacem@gmail.com>
2018-12-04 18:26:23 +0100

    MISC

--- init.sh
+++ init.sh
@@ -7,26 +7,18 @@
 # TODO
 
 # nginx root conf
-echo "/tmp/mytrala1"
 cp nginx_root_conf.conf /etc/nginx/nginx.conf
-echo "/tmp/mytrala2"
 
 # nginx novus conf
 #rm /etc/nginx/conf.d/default.conf
-#touch /tmp/mytrala3
 cp novus.conf /etc/nginx/conf.d/novus.conf
-echo "/tmp/mytrala3"
-cp back.ini /etc/uwsgi/apps-enabled/back.ini
-echo "/tmp/mytrala4"
+cp back.ini /etc/nginx/
 
 # socket file
 touch /tmp/back.sock
-echo "/tmp/mytrala5"
 chown www-data:www-data /tmp/back.sock
-echo "/tmp/mytrala6"
 chmod 770 /tmp/back.sock
-echo "/tmp/mytrala7"
 
 # Restart services
 service nginx restart
-#service uwsgi restart
+restart uwsgi
